a80613edf434e03f530a3de903fada89e51a7b96
Mickey <vickeylee.w@gmail.com>
2020-07-12 15:15:53 +1000

    Updates

---
+++ precache-manifest.c5d7d7147e7f82f46bb143720bcff611.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff7230845c48a1d11cea254467d88c95",
+    "url": "/Taiwan_Hotspots/index.html"
+  },
+  {
+    "revision": "e9216a5647f78e741ecb",
+    "url": "/Taiwan_Hotspots/static/css/main.cfb61989.chunk.css"
+  },
+  {
+    "revision": "fbc34d453f3f5fd9d272",
+    "url": "/Taiwan_Hotspots/static/js/2.18f4331c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Taiwan_Hotspots/static/js/2.18f4331c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9216a5647f78e741ecb",
+    "url": "/Taiwan_Hotspots/static/js/main.810ef1bc.chunk.js"
+  },
+  {
+    "revision": "d7974cfbad783c46ff67",
+    "url": "/Taiwan_Hotspots/static/js/runtime-main.3ef29b77.js"
+  }
+]);
